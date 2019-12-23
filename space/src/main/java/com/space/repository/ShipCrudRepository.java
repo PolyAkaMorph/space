1e2db663c93c06530a412c3ce5fc796d2efa2f0d
polyakamorph <polyakamorph@gmail.com>
2019-12-23 21:11:55 +0300

    git git

--- space/src/main/java/com/space/repository/ShipCrudRepository.java
+++ space/src/main/java/com/space/repository/ShipCrudRepository.java
@@ -3,14 +3,14 @@ package com.space.repository;
 import com.space.model.Ship;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShipCrudRepository extends CrudRepository<Ship,Long> {
-    Iterable<Ship> findByNameIgnoreCaseContainingAndPlanetIgnoreCaseContaining(String name, String planet);
 
-    @Query("from Ship s where(:name is null or s.name = :name)")
-    // and (:planet is null or s.planet = :planet)
-    Iterable<Ship> findShips(String name);
+    @Query(value = "from Ship where (:name is null or lower(name) like lower(concat('%', :name,'%'))) and " +
+    "(:planet is null or lower(planet) like lower(concat('%', :planet,'%')))")
+    Iterable<Ship> getShipsList(@Param("name") String name, @Param("planet") String planet);
 
 }
